e428d109547ccd145dea8c2807d9f48edff42bb7
rashmi <rashmimshewale11@gmail.com>
2018-10-08 17:55:47 +0530

    main_admin

--- src/com/java/UpdateDetails.java
+++ src/com/java/UpdateDetails.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-//                    <img src="data:image/png;base64,<%=Base64.getEncoder().encodeToString(set.getBytes(3))%>" class="b"/>
+// <img src="data:image/png;base64,<%=Base64.getEncoder().encodeToString(set.getBytes(3))%>" class="b"/>
 @WebServlet("/updateServlet")
 @MultipartConfig(maxFileSize = 16177215)
 public class UpdateDetails extends HttpServlet {
@@ -21,7 +21,7 @@ public class UpdateDetails extends HttpServlet {
         try {
             Connection con = myDB.getCon();
             PreparedStatement stmt = con.prepareStatement("update lab_time set from_time = ?, to_time=?, day = ? where lab_no=?");
-            PreparedStatement stmt1 = con.prepareStatement("update lab_details set capacity = ?,photo=? where lab_no = ?");
+            PreparedStatement stmt1 = con.prepareStatement("update lab_details set capacity = ? where lab_no = ?");
 
             stmt.setString(1, request.getParameter("lab_from_time"));
             stmt.setString(2, request.getParameter("lab_to_time"));
@@ -32,22 +32,22 @@ public class UpdateDetails extends HttpServlet {
             //    System.out.println(request.getParameter("lab_from_time"));
 
 
-            InputStream is = null;
-            Part fp = request.getPart("photo");
-            System.out.println(fp);
-            if (fp != null) {
-                System.out.println(fp.getName());
-                System.out.println(fp.getSize());
-                System.out.println(fp.getContentType());
+//            InputStream is = null;
+//            Part fp = request.getPart("photo");
+//            System.out.println(fp);
+//            if (fp != null) {
+//                System.out.println(fp.getName());
+//                System.out.println(fp.getSize());
+//                System.out.println(fp.getContentType());
 
 
-                is = fp.getInputStream();
+//                is = fp.getInputStream();
                 stmt1.setString(1, request.getParameter("lab_capacity"));
-                stmt1.setBlob(2, is);
-                stmt1.setString(3, request.getParameter("lab_name"));
+  //              stmt1.setBlob(2, is);
+                stmt1.setString(2, request.getParameter("lab_name"));
 
 
-            }
+        //    }
 
             int i = stmt.executeUpdate();
             int i1 = stmt1.executeUpdate();
---
+++ web/jsp/insert_lab_assistant.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rashmi
+  Date: 8/10/18
+  Time: 5:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.java.myDB" %>
+<%@ page import="java.sql.Connection" %>
+<%@ page import="java.sql.PreparedStatement" %>
+<%@ page import="java.sql.ResultSet" %>
+
+
+<%
+  Connection con = myDB.getCon();
+    String assistant_id = request.getParameter("id");
+    String assistant_name = request.getParameter("name");
+    String mob_no = request.getParameter("mob_no");
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    PreparedStatement stmt = con.prepareStatement("insert into assistant_info values(?,?,?,?,?)");
+    stmt.setString(1, assistant_id);
+    stmt.setString(2, assistant_name);
+    stmt.setString(3, mob_no);
+    stmt.setString(4, email);
+    stmt.setString(5, password);
+    stmt.executeUpdate();
+
+%>
+
